82e64a80e23bdecbcc13b0d39b46276480987aba
rahulsharma7-7 <rs1722678@gmail.com>
2026-02-02 06:14:13 +0000

    variables config done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,12 @@
+const accountId = 156425
+let accountEmail = "rs1722678@gmail.com"
+var accountPassword = "12354"
+accountCity = "jaipur"
+
+//accountId = 552546 not allowed
+console.log(accountId);
+accountEmail = "rock1722678@gmail.com"
+accountPassword = "55485"
+accountCity = "jodhpur"
+
+console.table([accountEmail,accountId,accountPassword,accountCity])
